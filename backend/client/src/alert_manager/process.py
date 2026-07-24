bd9b6137d1efdb2ec8f132f72aa74a0fd2bd3863
danilotec <danilo0123499@gmail.com>
2026-07-24 11:30:36 -0300

    ponto de orvalho

--- backend/client/src/alert_manager/process.py
+++ backend/client/src/alert_manager/process.py
@@ -10,7 +10,7 @@ class ProcessData:
         ("purity",            operator.lt,  90.0,  "Baixa Pureza: {value}%"),
         ("product_pressure",  operator.lt,   6.0,  "Baixa pressão do Produto: {value}"),
         ("pressure",          operator.lt,   6.8,  "Baixa Pressão da central: {value}"),
-        ("dew_point",         operator.gt, 5.0,  "Ponto de orvalho Alto: {value}"),
+        ("dew_point",         operator.gt, 50.0,  "Ponto de orvalho Alto: {value}"),
         ("line",              operator.lt,   6.0,  "Baixa pressão de Rede: {value}"),
     ]
 
@@ -22,7 +22,7 @@ class ProcessData:
     HOSPITAL_RULES = [
         ("pressure", operator.lt, 6.8, "Baixa Pressão da central: {value}"),
         ("line",     operator.lt, 6.0, "Baixa pressão de Rede: {value}"),
-        ("dew_point", operator.gt, 5.0, "Ponto de orvalho Alto: {value}"),
+        ("dew_point", operator.gt, 50.0, "Ponto de orvalho Alto: {value}"),
     ]
 
 
@@ -145,7 +145,7 @@ class Handles:
     def create_recover_message(cls, fault: Fault):
       
         return  (
-            f'ALERTA: Falta Resetada Pelo Sistema {fault.source} {fault.hospital}\n\n'
+            f'ALERTA: Falha Resetada Pelo Sistema {fault.source} {fault.hospital}\n\n'
             f'Identificação Da Falha:\n' 
             f'{fault.message}'
         )
